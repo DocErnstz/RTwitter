c411b13b0e33f0ecf5c2ea9b92923a73e8915e3e
DocErnstz <ernesto800@outlook.com>
2021-08-03 02:45:38 -0300

    action tweet route && getTweets route set up

---
+++ pages/api/tweets/actionTweet/[aid].ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "PUT") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  try {
+    const { aid } = req.query;
+    const property = req.body.updateProp;
+    const tweet = await prisma.tweet.findFirst({
+      where: {
+        id: String(aid),
+      },
+    });
+    let name;
+    switch (property) {
+      case "likes":
+        name = tweet.likes;
+        // code block
+        break;
+      case "retweets":
+        name = tweet.retweets;
+        // code block
+        break;
+      case "hearts":
+        name = tweet.hearts;
+        break;
+      default:
+      // code block
+    }
+
+    const updateTweet = await prisma.tweet.update({
+      where: {
+        id: String(aid),
+      },
+      data: {
+        [property]: name + 1,
+      },
+    });
+
+    res.status(200).json({ updateTweet });
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
+};
