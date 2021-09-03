df17362442dc732077e9f5cc4071529ae23cb00e
DocErnstz <ernesto800@outlook.com>
2021-09-03 14:02:49 -0300

    iconsfont

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,10 @@ import "../styles/index.css";
 import { AppProps } from "next/app";
 import { AuthProvider } from "../context/AuthContext";
 import React from "react";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import '@fortawesome/fontawesome-free/js/all.js';
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false //
 import Form from "../components/form/formTweet";
 
 function MyApp({ Component, pageProps }: AppProps) {
