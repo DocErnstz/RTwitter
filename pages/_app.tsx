193912cb289c71d1ddcb2466372f68246b0ed017
DocErnstz <ernesto800@outlook.com>
2021-09-05 14:03:09 -0300

    removed fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,7 @@ import { AppProps } from "next/app";
 import { AuthProvider } from "../context/AuthContext";
 import React from "react";
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import '@fortawesome/fontawesome-free/js/all.js';
+
 import Form from "../components/form/formTweet";
 
 function MyApp({ Component, pageProps }: AppProps) {
